f2171a1344a4f7c357e8f32e47e548052ff542b0
HP <bbcb@gmail.com>
2025-09-19 16:59:39 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,25 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 import 'home.dart';
-
 void main() {
-  runApp(const Home());
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const Home(),
+      theme: ThemeData(
+        fontFamily: "ArefRuqaa",
+        primaryColor: Colors.teal,
+        // باقي الثيم هنا
+      ),
+    );
+  }
 }
 
 
